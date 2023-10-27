e7fc2fb673ec94c9edace7ac9a9985743554ee0a
Nikifor Dragov <ndragov99@gmail.com>
2023-10-27 17:26:35 +0300

    Create Brands.jsx

---
+++ 1.Intro to React and JSX/First steps in react/first-steps/src/components/Brands.jsx
@@ -0,0 +1,51 @@
+export default function Brands() {
+    return(
+        <section id="brand" className="brand">
+    <div className="container">
+      <div className="brand-area">
+        <div className="owl-carousel owl-theme brand-item">
+          <div className="item">
+            <a href="#">
+              <img src="public/images/brand/br1.png" alt="brand-image" />
+            </a>
+          </div>
+          {/*/.item*/}
+          <div className="item">
+            <a href="#">
+              <img src="public/images/brand/br2.png" alt="brand-image" />
+            </a>
+          </div>
+          {/*/.item*/}
+          <div className="item">
+            <a href="#">
+              <img src="public/images/brand/br3.png" alt="brand-image" />on
+            </a>
+          </div>
+          {/*/.item*/}
+          <div className="item">
+            <a href="#">
+              <img src="public/images/brand/br4.png" alt="brand-image" />
+            </a>
+          </div>
+          {/*/.item*/}
+          <div className="item">
+            <a href="#">
+              <img src="public/images/brand/br5.png" alt="brand-image" />
+            </a>
+          </div>
+          {/*/.item*/}
+          <div className="item">
+            <a href="#">
+              <img src="public/images/brand/br6.png" alt="brand-image" />
+            </a>
+          </div>
+          {/*/.item*/}
+        </div>
+        {/*/.owl-carousel*/}
+      </div>
+      {/*/.clients-area*/}
+    </div>
+    {/*/.container*/}
+  </section>
+    );
+}
