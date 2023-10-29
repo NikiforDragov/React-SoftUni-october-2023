6baff95697f14f862e8c882bab02905070a8519c
Nikifor Dragov <ndragov99@gmail.com>
2023-10-29 11:17:36 +0200

    Add timer

--- 1.Intro to React and JSX/2.Components - Basic Idea/components-basics/src/components/Timer.jsx
+++ 1.Intro to React and JSX/2.Components - Basic Idea/components-basics/src/components/Timer.jsx
@@ -1,7 +1,18 @@
+import { useState } from 'react';
+
 export default function Timer(props) {
-    return(
+    const [time, setTime] = useState(0);
+
+    // Don't use setTimeout, useEffect is more appropriate!
+    setTimeout(() => {
+        setTime(time + 1)
+    }, 1000);
+
+    return (
         <div>
             <h3>Timer</h3>
+
+            <p>{time}</p>
         </div>
     );
 }
