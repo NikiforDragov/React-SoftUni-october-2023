dc3a878b3fb5113515c6e112215bb3efac2f461a
Nikifor Dragov <ndragov99@gmail.com>
2023-10-29 09:24:09 +0200

    Add movie component

---
+++ 1.Intro to React and JSX/2.Components - Basic Idea/components-basics/src/components/MovieList.jsx
@@ -0,0 +1,11 @@
+export default function MovieList(props) {
+    return(
+        <div className="movie-list">
+            <ul>
+                <li>First Movie</li>
+                <li>Second Movie</li>
+                <li>Third Movie</li>
+            </ul>
+        </div>
+    );
+}
