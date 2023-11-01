3ba2fb124272589ca59af000cdfcf2eee1eb3337
Nikifor Dragov <ndragov99@gmail.com>
2023-11-01 16:58:06 +0200

    Add header component

--- 3.Components Deep Dive/todo-list/client/TODO-LIST/src/App.jsx
+++ 3.Components Deep Dive/todo-list/client/TODO-LIST/src/App.jsx
@@ -1,125 +1,121 @@
+import Header from "./components/Header";
+
 function App() {
     return (
         <div>
-            <header class='navigation-header'>
-                <span class='navigation-logo'>
-                    <img src='/images/todo-icon.png' alt='todo-logo' />
-                </span>
-                <span class='spacer'></span>
-                <span class='navigation-description'>Todo List</span>
-            </header>
+            <Header />
 
-            <main class='main'>
-                <section class='todo-list-container'>
+            <main className='main'>
+                <section className='todo-list-container'>
                     <h1>Todo List</h1>
 
-                    <div class='add-btn-container'>
-                        <button class='btn'>+ Add new Todo</button>
+                    <div className='add-btn-container'>
+                        <button className='btn'>+ Add new Todo</button>
                     </div>
 
-                    <div class='table-wrapper'>
-                        <div class='loading-container'>
-                            <div class='loading-spinner'>
-                                <span class='loading-spinner-text'>
+                    <div className='table-wrapper'>
+                        <div className='loading-container'>
+                            <div className='loading-spinner'>
+                                <span className='loading-spinner-text'>
                                     Loading
                                 </span>
                             </div>
                         </div>
 
-                        <table class='table'>
+                        <table className='table'>
                             <thead>
                                 <tr>
-                                    <th class='table-header-task'>Task</th>
-                                    <th class='table-header-status'>Status</th>
-                                    <th class='table-header-action'>Action</th>
+                                    <th className='table-header-task'>Task</th>
+                                    <th className='table-header-status'>Status</th>
+                                    <th className='table-header-action'>Action</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr class='todo is-completed'>
+                                <tr className='todo is-completed'>
                                     <td>Give dog a bath</td>
                                     <td>Complete</td>
-                                    <td class='todo-action'>
-                                        <button class='btn todo-btn'>
+                                    <td className='todo-action'>
+                                        <button className='btn todo-btn'>
                                             Change status
                                         </button>
                                     </td>
                                 </tr>
 
-                                <tr class='todo is-completed'>
+                                <tr className='todo is-completed'>
                                     <td>Do laundry</td>
                                     <td>Complete</td>
-                                    <td class='todo-action'>
-                                        <button class='btn todo-btn'>
+                                    <td className='todo-action'>
+                                        <button className='btn todo-btn'>
                                             Change status
                                         </button>
                                     </td>
                                 </tr>
 
-                                <tr class='todo'>
+                                <tr className='todo'>
                                     <td>Vacuum floor</td>
                                     <td>Incomplete</td>
-                                    <td class='todo-action'>
-                                        <button class='btn todo-btn'>
+                                    <td className='todo-action'>
+                                        <button className='btn todo-btn'>
                                             Change status
                                         </button>
                                     </td>
                                 </tr>
 
-                                <tr class='todo is-completed'>
+                                <tr className='todo is-completed'>
                                     <td>Feed cat</td>
                                     <td>Complete</td>
-                                    <td class='todo-action'>
-                                        <button class='btn todo-btn'>
+                                    <td className='todo-action'>
+                                        <button className='btn todo-btn'>
                                             Change status
                                         </button>
                                     </td>
                                 </tr>
 
-                                <tr class='todo'>
+                                <tr className='todo'>
                                     <td>Change light bulbs</td>
                                     <td>Incomplete</td>
-                                    <td class='todo-action'>
-                                        <button class='btn todo-btn'>
+                                    <td className='todo-action'>
+                                        <button className='btn todo-btn'>
                                             Change status
                                         </button>
                                     </td>
                                 </tr>
 
-                                <tr class='todo is-completed'>
+                                <tr className='todo is-completed'>
                                     <td>Feed cat</td>
                                     <td>Complete</td>
-                                    <td class='todo-action'>
-                                        <button class='btn todo-btn'>
+                                    <td className='todo-action'>
+                                        <button className='btn todo-btn'>
                                             Change status
                                         </button>
                                     </td>
                                 </tr>
 
-                                <tr class='todo'>
+                                <tr className='todo'>
                                     <td>Change light bulbs</td>
                                     <td>Incomplete</td>
-                                    <td class='todo-action'>
-                                        <button class='btn todo-btn'>
+                                    <td className='todo-action'>
+                                        <button className='btn todo-btn'>
                                             Change status
                                         </button>
                                     </td>
                                 </tr>
 
-                                <tr class='todo is-completed'>
+                                <tr className='todo is-completed'>
                                     <td>Go to Store</td>
                                     <td>Completed</td>
-                                    <td class='todo-action'>
-                                        <button class='btn todo-btn'>
+                                    <td className='todo-action'>
+                                        <button className='btn todo-btn'>
                                             Change status
                                         </button>
                                     </td>
                                 </tr>
 
-                                <tr class='todo'>
+                                <tr className='todo'>
                                     <td>Fill gas tank</td>
                                     <td>Incomplete</td>
-                                    <td class='todo-action'>
-                                        <button class='btn todo-btn'>
+                                    <td className='todo-action'>
+                                        <button className='btn todo-btn'>
                                             Change status
                                         </button>
                                     </td>
@@ -130,7 +126,7 @@ function App() {
                 </section>
             </main>
 
-            <footer class='footer'>
+            <footer className='footer'>
                 <p>Copyright © designed by Mihail Valkov</p>
             </footer>
         </div>
