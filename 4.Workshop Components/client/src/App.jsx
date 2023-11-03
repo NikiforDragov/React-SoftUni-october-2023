993a9cd9502143797cbc50b881aebaa4e969e660
Nikifor Dragov <ndragov99@gmail.com>
2023-11-03 18:04:25 +0200

    Update components

--- 4.Workshop Components/client/src/App.jsx
+++ 4.Workshop Components/client/src/App.jsx
@@ -1,13 +1,14 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import './styles.css';
+import UserList from './components/UserList';
 
 function App() {
     return (
         <div>
             <Header />
             <main className="main">
-                
+                <UserList />
             </main>
             <Footer />
         </div>
