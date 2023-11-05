53246072fd65b76283310b7eba9b3e212c41da21
Nikifor Dragov <ndragov99@gmail.com>
2023-11-05 20:35:59 +0200

    Add spinner

--- 4.Workshop Components/client/src/components/UserListTable.jsx
+++ 4.Workshop Components/client/src/components/UserListTable.jsx
@@ -6,19 +6,23 @@ import UserListItem from './UserListItem';
 import CreateUserModal from './CreateUserModal';
 import UserDeleteModal from './UserDeleteModal';
 import UserInfoModal from './UserInfoModal';
+import Spinner from './Spinner';
 
 const UserListTable = () => {
     const [users, setUsers] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
     const [showCreate, setShowCreate] = useState(false);
     const [showInfo, setShowInfo] = useState(false);
     const [showDelete, setShowDelete] = useState(false);
     const [selectedUser, setSelectedUser] = useState(null);
 
     useEffect(() => {
+        setIsLoading(true);
         userService
             .getAll()
             .then((result) => setUsers(result))
-            .catch((err) => console.log(err));
+            .catch((err) => console.log(err))
+            .finally(() => setIsLoading(false));
     }, []);
 
     const createUserClickHandler = () => {
@@ -91,6 +95,7 @@ const UserListTable = () => {
                 />
             )}
 
+            {isLoading && <Spinner />}
             <table className='table'>
                 <thead>
                     <tr>
