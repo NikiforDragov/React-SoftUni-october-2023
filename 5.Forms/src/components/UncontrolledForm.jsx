c1cea8abbfc4901d97bd944e0af1ab2785d947fc
Nikifor Dragov <ndragov99@gmail.com>
2023-11-07 18:07:02 +0200

    Update UncontrolledForm.jsx

--- 5.Forms/src/components/UncontrolledForm.jsx
+++ 5.Forms/src/components/UncontrolledForm.jsx
@@ -1,5 +1,30 @@
+
 export default function UncontrolledForm() {
+    const submitHandler = (e) => {
+        e.preventDefault();
+    }
+
     return(
-        <h1>Uncontrolled form</h1>
+        <>
+            <h1>Uncontrolled form</h1>
+
+            <form onSubmit={submitHandler}>
+                <div>
+                    <label htmlFor="username">Username:</label>
+                    <input type="text" name="username" id="username" />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" name="password" id="password" />
+                </div>
+                <div>
+                    <label htmlFor="age">Age:</label>
+                    <input type="number" name="age" id="age" />
+                </div>
+                <div>
+                    <input type="submit" value='Register' />
+                </div>
+            </form>
+        </>
     );
 }
