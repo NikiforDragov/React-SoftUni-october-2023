06b2fcd1940d1c0b5a86f0c79c003b1cb3ded487
Nikifor Dragov <ndragov99@gmail.com>
2023-11-07 17:46:17 +0200

    Add to demo

---
+++ 5.Forms/src/App.jsx
@@ -0,0 +1,12 @@
+import UncontrolledForm from './components/UncontrolledForm';
+import './App.css';
+
+function App() {
+    return (
+        <div>
+            <UncontrolledForm />
+        </div>
+    );
+}
+
+export default App;
---
+++ 5.Forms/src/components/UncontrolledForm.jsx
@@ -0,0 +1,5 @@
+export default function UncontrolledForm() {
+    return(
+        <h1>Uncontrolled form</h1>
+    );
+}
