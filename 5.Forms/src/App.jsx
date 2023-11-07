c462bc6e1e391aec2c4a50d947a7fe507d14dfba
Nikifor Dragov <ndragov99@gmail.com>
2023-11-07 19:15:08 +0200

    Update App.jsx

--- 5.Forms/src/App.jsx
+++ 5.Forms/src/App.jsx
@@ -1,10 +1,11 @@
 import UncontrolledForm from './components/UncontrolledForm';
+import ControlledForm from './components/ControlledForm';
 import './App.css';
 
 function App() {
     return (
         <div>
-            <UncontrolledForm />
+            <ControlledForm />
         </div>
     );
 }
