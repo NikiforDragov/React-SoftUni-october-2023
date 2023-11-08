03def6672894aee0414e32c6e4d3bb895430535c
Nikifor Dragov <ndragov99@gmail.com>
2023-11-08 17:54:32 +0200

    Update App.jsx

--- 5.Forms/src/App.jsx
+++ 5.Forms/src/App.jsx
@@ -2,11 +2,21 @@ import UncontrolledForm from './components/UncontrolledForm';
 import ControlledFormRaw from './components/ControlledFormRaw';
 import ControlledForm from './components/ControlledForm';
 import './App.css';
+import { useRef } from 'react';
 
 function App() {
+    const resetButtonRef = useRef();
+
     return (
         <div>
-            <ControlledForm />
+            <ControlledForm buttonRef={resetButtonRef}/>
+
+            <button
+                type='submit'
+                ref={resetButtonRef}
+            >
+                Reset
+            </button>
         </div>
     );
 }
