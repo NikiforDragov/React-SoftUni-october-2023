bf2c6a2659edd521893031581804e7aca7666457
Nikifor Dragov <ndragov99@gmail.com>
2023-11-11 15:21:23 +0200

    Update App.jsx

--- 6.Routing/src/App.jsx
+++ 6.Routing/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Home from './components/Home';
 import About from './components/About';
+import Contacts from './components/Contacts';
 
 import './App.css';
 
@@ -15,6 +16,7 @@ function App() {
             <Routes>
                 <Route path='/' element={<Home />}></Route>
                 <Route path='/about' element={<About />}></Route>
+                <Route path='/contacts' element={<Contacts />}></Route>
             </Routes>
             <footer>All rights reserved</footer>
         </>
