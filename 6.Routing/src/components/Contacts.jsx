df69721fc423eb208a4e978770c6883e0573a489
Nikifor Dragov <ndragov99@gmail.com>
2023-11-11 15:21:16 +0200

    Create Contacts.jsx

---
+++ 6.Routing/src/components/Contacts.jsx
@@ -0,0 +1,13 @@
+const Contacts = () => {
+    return (
+        <>
+            <h2>Contacts Page</h2>
+            
+            <input type='text' />
+            <textarea name='' id='' cols='30' rows='10'></textarea>
+            <input type='submit' />
+        </>
+    );
+};
+
+export default Contacts;
