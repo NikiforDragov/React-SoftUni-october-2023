904158d948e0cb60879ee55f569469f4f14c04a5
Nikifor Dragov <ndragov99@gmail.com>
2023-11-11 15:16:29 +0200

    Create About.jsx

---
+++ 6.Routing/src/components/About.jsx
@@ -0,0 +1,18 @@
+const About = () => {
+    return (
+        <>
+            <h2>About Page</h2>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum
+                atque dignissimos suscipit exercitationem, mollitia, neque ea
+                voluptatem velit facere cumque adipisci sint culpa enim incidunt
+                ducimus saepe facilis, aliquam eum. Asperiores, consequatur sed
+                facere exercitationem tempore voluptatem expedita delectus
+                impedit, aliquam tenetur, numquam nostrum assumenda aut voluptas
+                porro vitae minima!
+            </p>
+        </>
+    );
+};
+
+export default About;
