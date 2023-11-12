15e3c54c05e0a1bc78ce1f81d3edb6686b03e599
Nikifor Dragov <ndragov99@gmail.com>
2023-11-12 14:56:34 +0200

    Create Values.jsx

---
+++ 6.Routing/src/components/Values.jsx
@@ -0,0 +1,18 @@
+const Values = () => {
+    return (
+        <>
+        <h3>Our Values</h3>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum
+                atque dignissimos suscipit exercitationem, mollitia, neque ea
+                voluptatem velit facere cumque adipisci sint culpa enim incidunt
+                ducimus saepe facilis, aliquam eum. Asperiores, consequatur sed
+                facere exercitationem tempore voluptatem expedita delectus
+                impedit, aliquam tenetur, numquam nostrum assumenda aut voluptas
+                porro vitae minima!
+            </p>
+        </>
+    );
+};
+
+export default Values;
