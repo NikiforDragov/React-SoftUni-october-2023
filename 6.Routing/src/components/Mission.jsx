e4a26930124cca4d27e266952b9fb99426abd92e
Nikifor Dragov <ndragov99@gmail.com>
2023-11-12 14:56:37 +0200

    Create Mission.jsx

---
+++ 6.Routing/src/components/Mission.jsx
@@ -0,0 +1,18 @@
+const Mission = () => {
+    return (
+        <>
+            <h3>Our Mission</h3>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum
+                atque dignissimos suscipit exercitationem, mollitia, neque ea
+                voluptatem velit facere cumque adipisci sint culpa enim incidunt
+                ducimus saepe facilis, aliquam eum. Asperiores, consequatur sed
+                facere exercitationem tempore voluptatem expedita delectus
+                impedit, aliquam tenetur, numquam nostrum assumenda aut voluptas
+                porro vitae minima!
+            </p>
+        </>
+    );
+};
+
+export default Mission;
