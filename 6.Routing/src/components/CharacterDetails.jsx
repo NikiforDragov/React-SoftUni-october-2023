7346b44a95d0c7e3f7cac855a99be7992bd07c15
Nikifor Dragov <ndragov99@gmail.com>
2023-11-12 13:17:21 +0200

    Update CharacterDetails.jsx

--- 6.Routing/src/components/CharacterDetails.jsx
+++ 6.Routing/src/components/CharacterDetails.jsx
@@ -1,9 +1,20 @@
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
 const CharacterDetails = () => {
+    const { id } = useParams();
+    const [character, setCharacter] = useState({});
     const name = 'Unknown';
+
+    useEffect(() => {
+        fetch(`https://swapi.dev/api/people/${id}`)
+            .then((res) => res.json())
+            .then(setCharacter);
+    }, [id]);
 
     return (
         <>
-            <h1>{name}</h1>
+            <h1>{character.name}</h1>
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
                 cumque vel, consectetur ab quos natus aliquid quidem omnis
