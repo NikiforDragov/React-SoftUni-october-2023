f756d1b1f290a606a6eb26fcd26ee75503059aaf
Nikifor Dragov <ndragov99@gmail.com>
2023-11-15 18:17:40 +0200

    Create Game Details component

--- 6.Routing/Games-play Workshop/client/src/App.jsx
+++ 6.Routing/Games-play Workshop/client/src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/header/Header';
 import Home from './components/home/Home';
 import GameList from './components/game-list/GameList';
 import GameCreate from './components/game-create/GameCreate';
+import GameDetails from './components/game-list/game-details/GameDetails';
 import Login from './components/login/Login';
 import Register from './components/register/Register';
 
@@ -16,6 +17,7 @@ function App() {
                 <Route path='/' element={<Home />}></Route>
                 <Route path='/games' element={<GameList />}></Route>
                 <Route path='/games/create' element={<GameCreate />}></Route>
+                <Route path='/games/:gameId' element={<GameDetails />}></Route>
                 <Route path='/login' element={<Login />}></Route>
                 <Route path='/register' element={<Register />}></Route>
             </Routes>
