d9b14183133b003192cb480a2331a80985d4c53c
Nikifor Dragov <ndragov99@gmail.com>
2023-11-16 15:01:01 +0200

    Add base comment func

--- 6.Routing/Games-play Workshop/client/src/services/commentService.js
+++ 6.Routing/Games-play Workshop/client/src/services/commentService.js
@@ -2,10 +2,16 @@ import * as request from '../lib/request';
 
 const baseUrl = 'http://localhost:3030/jsonstore/comments';
 
-export const create = async (gameId, userName, text) => {
+export const getAll = async () => {
+    const result = await request.get(baseUrl);
+
+    return Object.values(result);
+}
+
+export const create = async (gameId, username, text) => {
     const newComment = await request.post(baseUrl, {
         gameId,
-        userName,
+        username,
         text,
     });
 
