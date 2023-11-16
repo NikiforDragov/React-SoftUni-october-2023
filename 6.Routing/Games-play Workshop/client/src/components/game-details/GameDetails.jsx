d6571c97b50f753ea4ccdf10272d8591e8a5a462
Nikifor Dragov <ndragov99@gmail.com>
2023-11-16 14:48:34 +0200

    Add comments section

--- 6.Routing/Games-play Workshop/client/src/components/game-details/GameDetails.jsx
+++ 6.Routing/Games-play Workshop/client/src/components/game-details/GameDetails.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import * as gameService from '../../services/gameService';
+import * as commentService from '../../services/commentService';
 
 export default function GameDetails() {
     const [game, setGame] = useState({});
@@ -11,51 +12,75 @@ export default function GameDetails() {
         gameService.getOne(gameId).then(setGame);
     }, [gameId]);
 
+    const addCommentHandler = async (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.currentTarget);
+
+        const createdComment = await commentService.create(
+            gameId,
+            formData.get('username'),
+            formData.get('comment'),
+        );
+
+        console.log();
+    };
+
     return (
         <section id='game-details'>
             <h1>Game Details</h1>
             <div className='info-section'>
                 <div className='game-header'>
-                    <img className='game-img' src={game.imageUrl} />
+                    <img
+                        className='game-img'
+                        src={game.imageUrl}
+                        alt={game.title}
+                    />
                     <h1>{game.title}</h1>
-                    <span className='levels'>{game.maxLevel}</span>
+                    <span className='levels'>MaxLevel: {game.maxLevel}</span>
                     <p className='type'>{game.category}</p>
                 </div>
 
                 <p className='text'>{game.summary}</p>
 
-                {/* <!-- Bonus ( for Guests and Users ) -->
-                <div className="details-comments">
+                <div className='details-comments'>
                     <h2>Comments:</h2>
                     <ul>
-                        <!-- list all comments for current game (If any) -->
-                        <li className="comment">
+                        {/*  list all comments for current game (If any)  */}
+                        <li className='comment'>
                             <p>Content: I rate this one quite highly.</p>
                         </li>
-                        <li className="comment">
+                        <li className='comment'>
                             <p>Content: The best game.</p>
                         </li>
                     </ul>
-                    <!-- Display paragraph: If there are no games in the database -->
-                    <p className="no-comment">No comments.</p>
-                </div> */}
+                    {/* Display paragraph: If there are no games in the database  */}
+                    <p className='no-comment'>No comments.</p>
+                </div>
 
-                {/* <!-- Edit/Delete buttons ( Only for creator of this game )  --> */}
+                {/*  Edit/Delete buttons ( Only for creator of this game )   */}
                 {/* <div className="buttons">
                     <a href="#" className="button">Edit</a>
                     <a href="#" className="button">Delete</a>
                 </div> */}
             </div>
 
-            {/* <!-- Bonus -->
-            <!-- Add Comment ( Only for logged-in users, which is not creators of the current game ) -->
-            <article className="create-comment">
+            {/* Add Comment ( Only for logged-in users, which is not creators of the current game )  */}
+            <article className='create-comment'>
                 <label>Add new comment:</label>
-                <form className="form">
-                    <textarea name="comment" placeholder="Comment......"></textarea>
-                    <input className="btn submit" type="submit" value="Add Comment"/>
+                <form className='form' onSubmit={addCommentHandler}>
+                    <input type='text' name='username' placeholder='username' />
+                    <textarea
+                        name='comment'
+                        placeholder='Comment......'
+                    ></textarea>
+                    <input
+                        className='btn submit'
+                        type='submit'
+                        value='Add Comment'
+                    />
                 </form>
-            </article> */}
+            </article>
         </section>
     );
 }
