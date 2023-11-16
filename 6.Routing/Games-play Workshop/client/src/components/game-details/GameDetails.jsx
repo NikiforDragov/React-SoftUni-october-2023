5890e0de6aef167e559b5fd0b61d6b0f4e862b79
Nikifor Dragov <ndragov99@gmail.com>
2023-11-16 15:06:45 +0200

    Add new comment to local state

--- 6.Routing/Games-play Workshop/client/src/components/game-details/GameDetails.jsx
+++ 6.Routing/Games-play Workshop/client/src/components/game-details/GameDetails.jsx
@@ -26,7 +26,7 @@ export default function GameDetails() {
             formData.get('comment')
         );
 
-        console.log();
+        setComments((state) => [...state, createdComment]);
     };
 
     return (
@@ -49,8 +49,8 @@ export default function GameDetails() {
                 <div className='details-comments'>
                     <h2>Comments:</h2>
                     <ul>
-                        {comments.map(({ username, text }) => (
-                            <li className='comment'>
+                        {comments.map(({ _id, username, text }) => (
+                            <li key={_id} className='comment'>
                                 <p>
                                     {username}: {text}
                                 </p>
