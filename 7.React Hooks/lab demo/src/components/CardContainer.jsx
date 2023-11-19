47a8802f7d398be00cd2ff3a6ec07c0c5bb4e7e5
Nikifor Dragov <ndragov99@gmail.com>
2023-11-19 14:16:58 +0200

    Add styles for todos

--- 7.React Hooks/lab demo/src/components/CardContainer.jsx
+++ 7.React Hooks/lab demo/src/components/CardContainer.jsx
@@ -2,7 +2,7 @@ import ToDoCard from './ToDoCard';
 
 export default function CardContainer({ todos }) {
     return (
-        <div style={{ margin: '20px' }}>
+        <div style={{ margin: '20px', display: 'flex', flexDirection: 'row' }}>
             {todos.map((item) => (
                 <ToDoCard key={item._id} {...item} />
             ))}
