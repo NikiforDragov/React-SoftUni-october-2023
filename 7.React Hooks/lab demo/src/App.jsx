8578c6b3294e7bb332c00fa030f5b503ab6c76e2
Nikifor Dragov <ndragov99@gmail.com>
2023-11-19 13:57:28 +0200

    Fetch todos

--- 7.React Hooks/lab demo/src/App.jsx
+++ 7.React Hooks/lab demo/src/App.jsx
@@ -1,13 +1,28 @@
+import { useEffect, useState } from 'react';
 import CardContainer from './components/CardContainer';
 import Header from './components/Header';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const baseUrl = 'http://localhost:3030/jsonstore/';
+
 function App() {
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        fetch(`${baseUrl}todos`)
+            .then((response) => {
+                response.json();
+            })
+            .then((result) => {
+                setTodos(Object.values(result));
+            });
+    });
+
     return (
         <>
             <Header />
-            <CardContainer/>
+            <CardContainer todos={todos} />
         </>
     );
 }
--- 7.React Hooks/lab demo/src/components/CardContainer.jsx
+++ 7.React Hooks/lab demo/src/components/CardContainer.jsx
@@ -1,6 +1,6 @@
 import CardExample from './CardExample';
 
-export default function CardContainer() {
+export default function CardContainer(todos) {
     return (
         <div style={{ margin: '20px' }}>
             <CardExample />
