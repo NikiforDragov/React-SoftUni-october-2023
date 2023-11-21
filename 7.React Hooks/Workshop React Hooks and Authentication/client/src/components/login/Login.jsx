7e8585e356892b09185941555804cc6c97116b67
Nikifor Dragov <ndragov99@gmail.com>
2023-11-21 15:02:00 +0200

    Change login to controlled form

--- 7.React Hooks/Workshop React Hooks and Authentication/client/src/components/login/Login.jsx
+++ 7.React Hooks/Workshop React Hooks and Authentication/client/src/components/login/Login.jsx
@@ -1,9 +1,15 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import useForm from '../../hooks/useForm';
 
 export default function Login() {
+    const [values, onChange, onSubmit] = useForm({
+        email: '',
+        password: '',
+    });
+
     return (
         <section id='login-page' className='auth'>
-            <form id='login'>
+            <form id='login' onSubmit={onSubmit}>
                 <div className='container'>
                     <div className='brand-logo'></div>
                     <h1>Login</h1>
@@ -13,6 +19,8 @@ export default function Login() {
                         id='email'
                         name='email'
                         placeholder='Sokka@gmail.com'
+                        onChange={onChange}
+                        value={values.email}
                     />
 
                     <label htmlFor='login-pass'>Password:</label>
@@ -20,11 +28,14 @@ export default function Login() {
                         type='password'
                         id='login-password'
                         name='password'
+                        onChange={onChange}
+                        value={values.password}
                     />
                     <input type='submit' className='btn submit' value='Login' />
                     <p className='field'>
                         <span>
-                            If you don't have profile click <Link to='/register'>here</Link>
+                            If you don't have profile click{' '}
+                            <Link to='/register'>here</Link>
                         </span>
                     </p>
                 </div>
