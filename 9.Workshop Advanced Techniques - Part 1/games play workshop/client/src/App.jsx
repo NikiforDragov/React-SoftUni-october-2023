3829414e2bca1eab8b532939af70531894514cf2
Nikifor Dragov <ndragov99@gmail.com>
2023-11-28 14:39:53 +0200

    Init new demo

---
+++ 9.Workshop Advanced Techniques - Part 1/games play workshop/client/src/App.jsx
@@ -0,0 +1,43 @@
+import { Routes, Route } from 'react-router-dom';
+
+import { AuthProvider } from './contexts/authContext';
+import Path from './paths';
+
+import Header from './components/header/Header';
+import Home from './components/home/Home';
+import GameList from './components/game-list/GameList';
+import GameCreate from './components/game-create/GameCreate';
+import GameDetails from './components/game-details/GameDetails';
+import Login from './components/login/Login';
+import Register from './components/register/Register';
+import Logout from './components/logout/Logout';
+import GameEdit from './components/game-edit/GameEdit';
+
+function App() {
+    return (
+        <AuthProvider>
+            <div id='box'>
+                <Header />
+
+                <Routes>
+                    <Route path={Path.Home} element={<Home />}></Route>
+                    <Route path='/games' element={<GameList />}></Route>
+                    <Route
+                        path='/games/create'
+                        element={<GameCreate />}
+                    ></Route>
+                    <Route
+                        path='/games/:gameId/details'
+                        element={<GameDetails />}
+                    ></Route>
+                    <Route path={Path.GameEdit} element={<GameEdit />}></Route>
+                    <Route path='/login' element={<Login />}></Route>
+                    <Route path='/register' element={<Register />}></Route>
+                    <Route path={Path.Logout} element={<Logout />}></Route>
+                </Routes>
+            </div>
+        </AuthProvider>
+    );
+}
+
+export default App;
