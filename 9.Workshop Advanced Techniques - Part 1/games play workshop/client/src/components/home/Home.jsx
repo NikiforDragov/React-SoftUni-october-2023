37fd32ce5d65d50746eea9fe0de45580fee7e8ef
Nikifor Dragov <ndragov99@gmail.com>
2023-11-29 16:53:26 +0200

    Update Home.jsx

--- 9.Workshop Advanced Techniques - Part 1/games play workshop/client/src/components/home/Home.jsx
+++ 9.Workshop Advanced Techniques - Part 1/games play workshop/client/src/components/home/Home.jsx
@@ -1,6 +1,15 @@
+import { useEffect, useState } from 'react';
 import withAuth from '../../HOC/withAuth';
+import * as gameService from '../../services/gameService';
+import LatestGame from './latest-game/LatestGame';
 
 function Home({ _id, accessToken, email }) {
+    const [latestGames, setLatestGames] = useState([]);
+
+    useEffect(() => {
+        gameService.getLatest().then((result) => setLatestGames(result));
+    }, []);
+
     return (
         <section id='welcome-world'>
             <div className='welcome-message'>
@@ -12,64 +21,13 @@ function Home({ _id, accessToken, email }) {
             <div id='home-page'>
                 <h1>Latest Games</h1>
 
-                {/* <!-- Display div: with information about every game (if any) --> */}
-                <div className='game'>
-                    <div className='image-wrap'>
-                        <img src='./images/CoverFire.png' />
-                    </div>
-                    <h3>Cover Fire</h3>
-                    <div className='rating'>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                    </div>
-                    <div className='data-buttons'>
-                        <a href='#' className='btn details-btn'>
-                            Details
-                        </a>
-                    </div>
-                </div>
-                <div className='game'>
-                    <div className='image-wrap'>
-                        <img src='./images/ZombieLang.png' />
-                    </div>
-                    <h3>Zombie Lang</h3>
-                    <div className='rating'>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                    </div>
-                    <div className='data-buttons'>
-                        <a href='#' className='btn details-btn'>
-                            Details
-                        </a>
-                    </div>
-                </div>
-                <div className='game'>
-                    <div className='image-wrap'>
-                        <img src='./images/MineCraft.png' />
-                    </div>
-                    <h3>MineCraft</h3>
-                    <div className='rating'>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                    </div>
-                    <div className='data-buttons'>
-                        <a href='#' className='btn details-btn'>
-                            Details
-                        </a>
-                    </div>
-                </div>
+                {latestGames.map((game) => (
+                    <LatestGame key={game._id} {...game} />
+                ))}
 
-                {/* <!-- Display paragraph: If there is no games  --> */}
-                {/* <p className='no-articles'>No games yet</p> */}
+                {!latestGames.length && (
+                    <p className='no-articles'>No games yet</p>
+                )}
 
                 <p>{email}</p>
             </div>
