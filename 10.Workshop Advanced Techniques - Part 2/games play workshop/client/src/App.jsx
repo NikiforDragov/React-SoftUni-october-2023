170ed0400e711055277b1802167e4ed4e2ceeae3
Nikifor Dragov <ndragov99@gmail.com>
2023-11-30 22:03:14 +0200

    Add lazyloading

--- 10.Workshop Advanced Techniques - Part 2/games play workshop/client/src/App.jsx
+++ 10.Workshop Advanced Techniques - Part 2/games play workshop/client/src/App.jsx
@@ -1,3 +1,4 @@
+import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import { AuthProvider } from './contexts/authContext';
@@ -7,13 +8,14 @@ import Header from './components/header/Header';
 import Home from './components/home/Home';
 import GameList from './components/game-list/GameList';
 import GameCreate from './components/game-create/GameCreate';
-import GameDetails from './components/game-details/GameDetails';
 import Login from './components/login/Login';
 import Register from './components/register/Register';
 import Logout from './components/logout/Logout';
 import GameEdit from './components/game-edit/GameEdit';
 import ErrorBoundary from './components/ErrorBoundary';
 import AuthGuard from './components/guards/AuthGuard';
+// import GameDetails from './components/game-details/GameDetails';
+const GameDetails = lazy(() => import('./components/game-details/GameDetails'));
 
 function App() {
     return (
@@ -21,29 +23,36 @@ function App() {
             <AuthProvider>
                 <div id='box'>
                     <Header />
-
-                    <Routes>
-                        <Route path={Path.Home} element={<Home />}></Route>
-                        <Route path='/games' element={<GameList />}></Route>
-                        <Route
-                            path='/games/:gameId/details'
-                            element={<GameDetails />}
-                        ></Route>
-
-                        <Route path='/login' element={<Login />}></Route>
-                        <Route path='/register' element={<Register />}></Route>
-                        <Route element={<AuthGuard />}>
+                    <Suspense fallback={<h1>Loading...</h1>}>
+                        <Routes>
+                            <Route path={Path.Home} element={<Home />}></Route>
+                            <Route path='/games' element={<GameList />}></Route>
                             <Route
-                                path='/games/create'
-                                element={<GameCreate />}
-                            />
-                        </Route>
-                        <Route
-                            path={Path.GameEdit}
-                            element={<GameEdit />}
-                        ></Route>
-                        <Route path={Path.Logout} element={<Logout />}></Route>
-                    </Routes>
+                                path='/games/:gameId/details'
+                                element={<GameDetails />}
+                            ></Route>
+
+                            <Route path='/login' element={<Login />}></Route>
+                            <Route
+                                path='/register'
+                                element={<Register />}
+                            ></Route>
+                            <Route element={<AuthGuard />}>
+                                <Route
+                                    path='/games/create'
+                                    element={<GameCreate />}
+                                />
+                            </Route>
+                            <Route
+                                path={Path.GameEdit}
+                                element={<GameEdit />}
+                            ></Route>
+                            <Route
+                                path={Path.Logout}
+                                element={<Logout />}
+                            ></Route>
+                        </Routes>
+                    </Suspense>
                 </div>
             </AuthProvider>
         </ErrorBoundary>
